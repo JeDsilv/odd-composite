81afefef24082370f0d16fecb06f0f64b8bba01b
JeDsilv <jeffedsilva@hotmail.com>
2025-04-19 23:15:31 -0300

    Create OnonP1.c

---
+++ OnonP1.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+// Criado por: J. D Silva
+// Propósito: Dirty Code - Apenas para aprendizado
+
+void gerar_impares_compostos(int limite) {
+    int m = 1, k = 0;
+    
+    printf("Numeros impares compostos:\n");
+    
+    for (int i = 0; i < limite; i++) {
+        int N = (2 * m + 1) * (2 * k + 1);
+        printf("%d ", N);
+        
+        k++; 
+        if (k > m) { // Quando k alcança m, reinicia e incrementa m
+            m++;
+            k = 1;
+        }
+    }
+    
+    printf("\n");
+}
+
+int main() {
+    int quantidade;
+    
+    printf("Digite a quantidade de numeros impares compostos a serem gerados: ");
+    scanf("%d", &quantidade);
+    
+    gerar_impares_compostos(quantidade);
+    
+    return 0;
+}
